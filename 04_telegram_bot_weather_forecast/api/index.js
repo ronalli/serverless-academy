b580391cd1d860becf08791c4b3d60da9b69b840
Nikita Khakim <khakimov.nikita.vch@gmail.com>
2023-10-26 23:15:06 +0300

    Update index.js

--- 04_telegram_bot_weather_forecast/api/index.js
+++ 04_telegram_bot_weather_forecast/api/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const LAT = '48.450001';
 const LON = '34.983334';
 
-const API_KEY = '47c4d92b4ce85d315e3dc2fb08f979b7';
+const API_KEY = process.env.API_KEY
 const API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${LAT}&lon=${LON}&appid=${API_KEY}`;
 
 export const getWeather = async () => {
